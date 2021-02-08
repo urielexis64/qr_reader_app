43d958054a90b149be135b0637e25f8c68746c16
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-07 21:37:22 -0700

    custom theme, home route

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:qrreaderapp/src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'QRReader', home: null);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.indigo[900]),
+      title: 'QRReader',
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext context) => HomePage(),
+      },
+    );
   }
 }
