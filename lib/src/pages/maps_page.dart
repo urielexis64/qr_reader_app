672187581bab969d37378788032ba647ba92dfaa
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-08 21:15:45 -0700

    Dissmisible widget

--- lib/src/pages/maps_page.dart
+++ lib/src/pages/maps_page.dart
@@ -19,10 +19,20 @@ class MapsPage extends StatelessWidget {
           return ListView.builder(
               itemCount: scans.length,
               itemBuilder: (context, i) {
-                return ListTile(
-                  leading: Icon(Icons.cloud),
-                  title: Text(scans[i].value),
-                  trailing: Icon(Icons.keyboard_arrow_right),
+                return Dismissible(
+                  key: UniqueKey(),
+                  onDismissed: (dir) {
+                    DBProvider.db.deleteScan(scans[i].id);
+                  },
+                  background: Container(
+                    color: Colors.redAccent,
+                  ),
+                  child: ListTile(
+                    leading: Icon(Icons.cloud),
+                    title: Text(scans[i].value),
+                    subtitle: Text('ID: ${scans[i].id}'),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                  ),
                 );
               });
         }
