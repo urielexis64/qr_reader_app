2bc54a00b35f06698f0d1998a4fbf6962b629edf
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-09 16:54:05 -0700

    map route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qrreaderapp/src/pages/home_page.dart';
+import 'package:qrreaderapp/src/pages/map_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
       initialRoute: 'home',
       routes: {
         'home': (BuildContext context) => HomePage(),
+        'map': (BuildContext context) => MapPage(),
       },
     );
   }
