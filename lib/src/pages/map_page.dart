f87d0e6679fd4c2282a4bf91e6bf4bf18d378ac6
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-09 21:51:47 -0700

    markers, floatingbutton and stateful widget

--- lib/src/pages/map_page.dart
+++ lib/src/pages/map_page.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:qrreaderapp/src/models/scan_model.dart';
-import "package:latlong/latlong.dart";
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
   final token =
       'pk.eyJ1IjoidXJpZWxleGlzNjQiLCJhIjoiY2treW5zd3BiMDg4dzJvcGx3OWYyZ201dyJ9.vFWYPMn2ZPKRA6hWfdXUMQ';
+
+  final mapController = new MapController();
+
+  int _styleIdIndex = 0;
+
+  final List<String> _styles = [
+    'streets-v11',
+    'outdoors-v11',
+    'light-v10',
+    'dark-v10',
+    'satellite-streets-v11',
+    'satellite-v9'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -17,21 +34,23 @@ class MapPage extends StatelessWidget {
         actions: [
           IconButton(
             icon: Icon(Icons.my_location),
-            onPressed: () {},
+            onPressed: () {
+              mapController.moveAndRotate(scan.getLatLng(), 18, 0);
+            },
           )
         ],
       ),
       body: _createFlutterMap(scan),
+      floatingActionButton: _createFloatingButton(context),
     );
   }
 
   Widget _createFlutterMap(ScanModel scan) {
     return FlutterMap(
+      mapController: mapController,
       options: MapOptions(
-        center: scan.getLatLng(),
-        zoom: 18.0,
-      ),
-      layers: [_createMap()],
+          center: scan.getLatLng(), zoom: 18.0, minZoom: 2, maxZoom: 30.0),
+      layers: [_createMap(), _createMarkers(scan)],
     );
   }
 
@@ -39,13 +58,40 @@ class MapPage extends StatelessWidget {
     return TileLayerOptions(
         urlTemplate:
             'https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
-        additionalOptions: {'accessToken': token, "id": 'satellite-v9'});
+        additionalOptions: {
+          'accessToken': token,
+          "id": '${_styles[_styleIdIndex]}'
+        });
   }
 
-  /*mapbox://styles/mapbox/streets-v11
-Mapbox Outdoors	mapbox://styles/mapbox/outdoors-v11
-Mapbox Light	mapbox://styles/mapbox/light-v10
-Mapbox Dark	mapbox://styles/mapbox/dark-v10
-Mapbox Satellite	mapbox://styles/mapbox/satellite-v9
-Mapbox Satellite Streets	mapbox://styles/mapbox/satellite-streets-v11 */
+  _createMarkers(ScanModel scan) {
+    return MarkerLayerOptions(markers: [
+      Marker(
+          width: 100,
+          height: 100,
+          point: scan.getLatLng(),
+          builder: (context) => Container(
+                  child: Icon(
+                Icons.location_on,
+                size: 70,
+                color: Theme.of(context).primaryColor,
+              )))
+    ]);
+  }
+
+  Widget _createFloatingButton(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(Icons.repeat),
+      backgroundColor: Theme.of(context).primaryColor,
+      onPressed: () {
+        setState(() {
+          if (_styleIdIndex == _styles.length - 1) {
+            _styleIdIndex = 0;
+          } else {
+            _styleIdIndex++;
+          }
+        });
+      },
+    );
+  }
 }
